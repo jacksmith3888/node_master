95c8f3ff51fd1bf39ae313ed7b32d40f00aaccb3
fukafu <fool199009170912@gmail.com>
2019-01-13 20:05:16 +0800

    feat: cp1

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,65 @@
-// export * from './lib/async';
-// export * from './lib/hash';
-// export * from './lib/number';
-
-
 import http from 'http'
 import url from 'url'
+import { StringDecoder } from 'string_decoder'
 
 const server = http.createServer((req, res) => {
   // Get and Parse URL
-  const parsedUrl = url.parse(req.url, true)
   // when we set the second param to true, it will parse the query string
+  const parsedUrl = url.parse(req.url, true)
+  // Get path
   const path = parsedUrl.pathname
   const trimmedPath = path.replace(/^\/+|\/+$/g, '')
-  console.log(trimmedPath);
-  console.log('abcabc');
+  // Get the querystring
 
-  // Get path
+  const queryString = parsedUrl.query
 
-  // Send response
-  res.end('Hello World\n')
+  const method = req.method.toLowerCase()
+  // console.log(`request is received on ${trimmedPath},with method: ${method}`)
 
-  // Log request path
-  // console.log('Request received on path: ' + trimmedPath);
+  const headers = req.headers
+
+  const decoder = new StringDecoder('utf-8')
+  let buffer:string = ''
+  req.on('data', (data) => {
+    buffer += decoder.write(data)
+  })
+
+  req.on('end', () => {
+    buffer += decoder.end()
+
+    const chosenHandler = router[trimmedPath] ? router[trimmedPath] : handlers.notFound
+    const data = {
+      trimmedPath,
+      queryString,
+      method,
+      headers,
+      payload:buffer
+    }
+    chosenHandler(data,(statusCode:number = 200, payload = {}) => {
+      const payloadString = JSON.stringify(payload)
+      res.writeHead(statusCode)
+
+      // Send response
+      res.end(payloadString)
+      console.log('returning',payloadString)
+
+    })
+  })
 })
 
 server.listen(3001, function() {
-  console.log('the server is listening on 3001 now');
+  console.log('the server is listening on 3001 now')
 })
+
+const handlers = {
+  sample: (data, cb) => {
+    cb(406, { name: 'sample'})
+  },
+  notFound: (data, cb) => {
+    cb(404)
+  }
+}
+
+const router = {
+  sample: handlers.sample
+}
